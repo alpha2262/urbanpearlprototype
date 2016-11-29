38b5e484759a445a8444051f07cfc2e06c5586e8
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-11-29 14:51:50 -0500

    reworked struture of components

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import {Header, Card} from './components/common';
+
+//import firebase
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Urban Pearl up and running!</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/MainPage.js
@@ -0,0 +1,1 @@
+//add in content for once logged in (or skipping login? check planning doc)
